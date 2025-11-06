8887a16530ce0b30966f4b4144f8b5540b40745d
mylu0x <mylu0x@gmail.com>
2025-11-06 15:52:06 +0900

    chore(cfg): setup nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,29 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts', '@nuxt/icon', '@nuxt/image', '@unocss/nuxt']
-})
+  modules: ['@nuxt/fonts', '@nuxt/icon', '@nuxt/image', '@unocss/nuxt'],
+  app: {
+    head: {
+      title: 'mylu0x',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ]
+    }
+  },
+  components: {
+    dirs: [
+      {
+        path: '~/components/',
+        pathPrefix: false
+      }
+    ]
+  },
+  css: ['~/assets/css/main.css'],
+  fonts: {
+    defaults: {
+      weights: [400, 500, 600, 700, 800],
+      styles: ['normal', 'italic', 'oblique']
+    }
+  }
+});
