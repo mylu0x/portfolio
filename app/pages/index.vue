fea2f0267ab0a8a82c2bb5e21cf0571ebc4f5b5a
mylu0x <mylu0x@gmail.com>
2025-11-07 13:18:05 +0900

    style(home): adjust spacing between sections

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,7 +5,7 @@ const lang = ref<langs>('en');
 </script>
 
 <template>
-  <section class="my-42px flex flex-col gap-5px">
+  <section class="my-42px flex flex-col gap-8px">
     <h1>
       <span class="text-38px font-600 text-shadow-sm mr-16px">Hey There!</span>
       <span class="text-14px font-500 text-gray-7">
@@ -20,7 +20,7 @@ const lang = ref<langs>('en');
         <button @click="lang = 'fr'">Français</button>
       </span>
     </h1>
-    <div class="flex flex-col gap-12px">
+    <div class="flex flex-col gap-16px">
       <template v-if="lang === 'en'">
         <p class="text-slate-7">
           <strong>Hey!</strong> My name is <strong>Mylu</strong> <small class="text-slate-6">(pronounced as /mɨlu/)</small>
