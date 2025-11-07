905afb46765752d131dcec21cf34c82dd56bcec7
mylu0x <mylu0x@gmail.com>
2025-11-07 10:29:24 +0900

    feat(footer): add external links

--- app/components/layout/Footer.vue
+++ app/components/layout/Footer.vue
@@ -9,10 +9,10 @@
       <span class="text-slate-8 font-600">Mylu</span>
     </div>
     <p class="text-14px font-500 text-gray-8 hover:text-black transition-all-100 ml-3px">&copy; 2025 Mylu. All Rights Reserved.</p>
-    <p class="mt-1px text-14px flex items-center gap-4px font-500 [&_a]:(b-b-(dashed 1px gray-3) text-gray-8 leading-none py-1px mx-4px transition-all-50)">
-      <a href="https://github.com/mylu0x" target="_blank" rel="noopener noreferrer" class="hover:text-black">GitHub</a>
-      <a href="https://misskey.io/@mylu0x" target="_blank" rel="noopener noreferrer" class="hover:text-black">Misskey.io</a>
-      <a href="https://zenn.dev/mylu0x" target="_blank" rel="noopener noreferrer" class="hover:text-black">Zenn</a>
+    <p class="mt-1px text-14px flex items-center gap-4px font-500 [&_a]:(b-b-(dotted 1px gray-4) text-gray-8 leading-none py-1px mx-4px transition-all-50)">
+      <a href="https://github.com/mylu0x" target="_blank" rel="noopener noreferrer" class="hover:text-black flex items-center gap-1px">GitHub<Icon name="tabler:arrow-up-right" class="size-14px text-gray-7" /></a>
+      <a href="https://misskey.io/@mylu0x" target="_blank" rel="noopener noreferrer" class="hover:text-black flex items-center gap-1px">Misskey.io<Icon name="tabler:arrow-up-right" class="size-14px text-gray-7" /></a>
+      <a href="https://zenn.dev/mylu0x" target="_blank" rel="noopener noreferrer" class="hover:text-black flex items-center gap-1px">Zenn<Icon name="tabler:arrow-up-right" class="size-14px text-gray-7" /></a>
     </p>
   </footer>
 </template>
